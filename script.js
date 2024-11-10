f4579ef34a3cc5a6275ae1da83b570e87429ddf8
tulieu-hbt <tulieugiangday.hbt@gmail.com>
2024-11-11 00:04:37 +0700

    Update script.js

--- script.js
+++ script.js
@@ -61,17 +61,18 @@ async function predict() {
     const predictions = await model.predict(inputTensor).data();
     const classLabels = ["thanh long", "chuối", "cà chua", "nho", "chanh"];
     const preservationTexts = {
-        "thanh long": "Bảo quản thanh long ở nơi thoáng mát...",
-        "chuối": "Chuối nên được bảo quản ở nhiệt độ phòng sau khi thu hoạch. Bạn có thể tham khảo về cách trồng chuối ...",
-        "cà chua": " Cà chua là một loại thực phẩm không chỉ ngon miệng mà còn rất giàu dinh dưỡng, mang lại nhiều lợi ích cho sức khỏe. 
-                     Cà chua chứa nhiều vitamin C, vitamin K, kali, và folate, rất tốt cho sức khỏe. 
-                     Đặc biệt, cà chua là nguồn cung cấp dồi dào chất chống oxy hóa lycopene, giúp giảm nguy cơ mắc các bệnh tim mạch và ung thư. 
-                     Hàm lượng nước và chất xơ trong cà chua cũng giúp cải thiện tiêu hóa, duy trì độ ẩm cho da và hỗ trợ quá trình giảm cân. 
-                     Bên cạnh đó, cà chua còn có tác dụng làm đẹp da, làm chậm quá trình lão hóa và tăng cường hệ miễn dịch. 
-                     Bạn có thể tham khảo về cách trồng cà chua để bổ sung bữa cơm gia đình ... ",
-        "nho": "Nho nên được bảo quản trong tủ lạnh...",
-        "chanh": "Chanh nên được bảo quản ở nhiệt độ phòng..."
-    };
+    "thanh long": "Bảo quản thanh long ở nơi thoáng mát...",
+    "chuối": "Chuối nên được bảo quản ở nhiệt độ phòng sau khi thu hoạch. Bạn có thể tham khảo về cách trồng chuối ...",
+    "cà chua": "Cà chua là một loại thực phẩm không chỉ ngon miệng mà còn rất giàu dinh dưỡng, mang lại nhiều lợi ích cho sức khỏe. \
+                 Cà chua chứa nhiều vitamin C, vitamin K, kali, và folate, rất tốt cho sức khỏe. \
+                 Đặc biệt, cà chua là nguồn cung cấp dồi dào chất chống oxy hóa lycopene, giúp giảm nguy cơ mắc các bệnh tim mạch và ung thư. \
+                 Hàm lượng nước và chất xơ trong cà chua cũng giúp cải thiện tiêu hóa, duy trì độ ẩm cho da và hỗ trợ quá trình giảm cân. \
+                 Bên cạnh đó, cà chua còn có tác dụng làm đẹp da, làm chậm quá trình lão hóa và tăng cường hệ miễn dịch. \
+                 Bạn có thể tham khảo về cách trồng cà chua để bổ sung bữa cơm gia đình ... ",
+    "nho": "Nho nên được bảo quản trong tủ lạnh...",
+    "chanh": "Chanh nên được bảo quản ở nhiệt độ phòng..."
+};
+
 
     let maxProbability = 0;
     let predictedClass = "";
