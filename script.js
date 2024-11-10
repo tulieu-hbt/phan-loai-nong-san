556e943d3e5ab72cda7b6aebdeb6cc466132313b
tulieu-hbt <tulieugiangday.hbt@gmail.com>
2024-11-11 00:33:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function captureImage() {
 }
 
 // Dự đoán nông sản
-async function predict() {
+/*async function predict() {
     if (!model) return;
 
     captureImage();
@@ -71,7 +71,46 @@ async function predict() {
                 Bạn có thể tham khảo về cách trồng cà chua để bổ sung bữa cơm gia đình ... ",
     "nho": "Nho nên được bảo quản trong tủ lạnh...",
     "chanh": "Chanh nên được bảo quản ở nhiệt độ phòng..."
+};*/
+const preservationTexts = {
+    "thanh long": "Bảo quản thanh long ở nơi thoáng mát...",
+    "chuối": "Chuối nên được bảo quản ở nhiệt độ phòng sau khi thu hoạch. Bạn có thể tham khảo về cách trồng chuối ...",
+    "cà chua": "Cà chua là một loại thực phẩm không chỉ ngon miệng mà còn rất giàu dinh dưỡng, mang lại nhiều lợi ích cho sức khỏe. \
+                Cà chua chứa nhiều vitamin C, vitamin K, kali, và folate, rất tốt cho sức khỏe. \
+                Đặc biệt, cà chua là nguồn cung cấp dồi dào chất chống oxy hóa lycopene, giúp giảm nguy cơ mắc các bệnh tim mạch và ung thư. \
+                Hàm lượng nước và chất xơ trong cà chua cũng giúp cải thiện tiêu hóa, duy trì độ ẩm cho da và hỗ trợ quá trình giảm cân. \
+                Bên cạnh đó, cà chua còn có tác dụng làm đẹp da, làm chậm quá trình lão hóa và tăng cường hệ miễn dịch. \
+                Bạn có thể tham khảo về cách trồng cà chua để bổ sung bữa cơm gia đình ... ",
+    "nho": "Nho nên được bảo quản trong tủ lạnh...",
+    "chanh": "Chanh nên được bảo quản ở nhiệt độ phòng..."
 };
+
+async function predict() {
+    if (!model) return;
+
+    captureImage();
+    const image = tf.browser.fromPixels(canvas);
+    const resizedImage = tf.image.resizeBilinear(image, [224, 224]);
+    const normalizedImage = resizedImage.div(255.0);
+    const inputTensor = tf.expandDims(normalizedImage, 0);
+
+    const predictions = await model.predict(inputTensor).data();
+    const classLabels = ["thanh long", "chuối", "cà chua", "nho", "chanh"];
+    const maxProbability = Math.max(...predictions);
+    const predictedClass = classLabels[predictions.indexOf(maxProbability)];
+
+    if (maxProbability < 0.7) {
+        result.innerText = "Không nhận diện được nông sản.";
+        preservationInfo.innerText = "";
+        return;
+    }
+
+    result.innerText = `Kết quả: ${predictedClass}`;
+    preservationInfo.innerHTML = `<div class="introduction">
+        <h3>${predictedClass}</h3>
+        <p>${preservationTexts[predictedClass]}</p>
+    </div>`;
+}
 
 
 
